ecc7cd9d87811843d40d32b4f19ca358030c50ab
themindasrimal <themindasrimal@gmail.com>
2021-08-15 23:26:44 +0530

    signin title

---
+++ lib/views/signin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:quizapp/widgets/widgets.dart';
+
+class SignIn extends StatelessWidget {
+  const SignIn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: appBar(context),
+      ),
+    );
+  }
+}
---
+++ lib/widgets/widgets.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+Widget appBar(BuildContext context) {
+  return RichText(
+    text: const TextSpan(
+      style: TextStyle(fontSize: 22),
+      children: <TextSpan>[
+        TextSpan(
+            text: 'Quiz',
+            style:
+                TextStyle(fontWeight: FontWeight.w500, color: Colors.black54)),
+        TextSpan(text: 'App', style: TextStyle(fontWeight: FontWeight.bold)),
+      ],
+    ),
+  );
+}
